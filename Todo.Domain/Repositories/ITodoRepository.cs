8b8e50fb56606d04fbd32e81228f0d45bc8a5131
lucas.valenzuela <lucas.valenzuela@squadra.com.br>
2020-11-26 06:57:04 -0300

    Aula 20 - Comaçando o Handler

---
+++ Todo.Domain/Repositories/ITodoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Todo.Domain.Entities;
+
+namespace Todo.Domain.Repositories
+{
+    public interface ITodoRepository
+    {
+        void Create(TodoItem todo);
+        void Update(TodoItem todo);
+    }
+}
